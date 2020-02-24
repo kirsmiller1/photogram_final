07cd3740fb3bd26de6cfa4d6d5fe891c345e7d45
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:22:14 +0000

    Direct association between users and follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,9 @@
 class Follow < ApplicationRecord
   # Direct associations
 
+  belongs_to :sender,
+             :class_name => "User"
+
   belongs_to :receiver,
              :class_name => "User",
              :foreign_key => "recipient_id"

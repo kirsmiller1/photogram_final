862405e956ed5787c49e2d2e1b885c216e3a9dd1
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:22:14 +0000

    Direct association between users and follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,10 @@
 class Follow < ApplicationRecord
   # Direct associations
 
+  belongs_to :receiver,
+             :class_name => "User",
+             :foreign_key => "recipient_id"
+
   # Indirect associations
 
   # Validations
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Follow, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:receiver) }
+
     end
 
     describe "InDirect Associations" do

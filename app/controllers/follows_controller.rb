73d0b322518f9739b2d2936542c90d4c20bc3e65
Raghu Betina <raghu@firstdraft.com>
2020-02-24 16:22:58 +0000

    Only follow user can modify follow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,4 +1,14 @@
 class FollowsController < ApplicationController
+  before_action :current_user_must_be_follow_receiver, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_follow_receiver
+    follow = Follow.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == follow.receiver
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @follows = Follow.all
 
